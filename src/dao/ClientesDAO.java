29b90e40c396831a1aaa116dda4028d91018543c
biancaferrazdejesus <dualservapp@gmail.com>
2023-09-30 19:55:55 -0300

    primeiro envio dos arquibos

---
+++ src/dao/ClientesDAO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author marcelbenitezdasilva
+ */
+public class ClientesDAO extends DAO_Abstract {
+
+    @Override
+    public void insert(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void update(Object object) {
+        session.beginTransaction();
+        session.flush();
+        session.clear();
+        session.save(object);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public Object list(int id) {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Grupo.class);
+        criteria.add(Restrictions.eq("id", id));
+        List lista = criteria.list();
+        session.getTransaction().commit();        
+        return lista;
+        
+    }
+
+    @Override
+    public List listAll() {
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Grupo.class);
+        List lista = criteria.list();
+        session.getTransaction().commit();        
+        return lista;
+        
+    }
+}
---
+++ src/dao/UsuariosDAO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Criteria;
+
+/**
+ *
+ * @author u10840155107
+ */
+public class UsuariosDAO extends DAO_Abstract{
+
+    @Override
+    public void insert(Object objeto) {
+      session.beginTransaction();
+      session.save(objeto);
+      session.getTransaction().commit();
+}
+
+    @Override
+    public void update(Object object) {
+        session.beginTransaction();
+        session.save(object);
+        session.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(Object object) {
+       session.beginTransaction();
+       session.flush();
+       session.clear();
+       session.delete(object);
+       session.getTransaction().commit();
+    }
+
+    @Override
+    public Object list(int id) {
+        session.beginTransaction();
+        
+        session.getTransaction().commit();
+        return null;
+    }
+
+    @Override
+    public ArrayList listAll() {
+   session.beginTransaction();
+    Criteria criteria = session.createCriteria(Grupo.class);
+    List lista = criteria.list();
+    session.getTransaction().commit();
+return (ArrayList) lista;
+}
+    
+}
